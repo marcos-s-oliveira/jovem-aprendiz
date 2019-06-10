bbc5e3fa6e7b4112734826b055b8562e8c30f2b1
Marcos Oliveira <marcos.oliveira098@gmail.com>
2019-06-10 16:31:44 -0400

    publicar

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -11,7 +11,7 @@ class user{
 
         require_once (views."template/theme-header.phtml");
         require_once (views."template/nav.phtml");
-
+        require_once (views."template/user-inscricoes.phtml");
         require_once (views."template/footer.phtml");
     }
 }
