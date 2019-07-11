f2374f701a99fde60f624a4943b45f446e9a5b61
Marcos Oliveira <marcos.oliveira098@gmail.com>
2019-07-10 21:57:49 -0400

    Upload de Curriculo

--- app/controllers/user.php
+++ app/controllers/user.php
@@ -14,4 +14,10 @@ class user{
         require_once (views."template/user-inscricoes.phtml");
         require_once (views."template/footer.phtml");
     }
+    public function curriculo(){
+        require_once (views."template/theme-header.phtml");
+        require_once (views."template/nav.phtml");
+        require_once (views."template/user-curriculo.phtml");
+        require_once (views."template/footer.phtml");
+    }
 }
